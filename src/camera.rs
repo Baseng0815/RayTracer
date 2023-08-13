623bf0f58c5f91af575592a64eef27ed6b996d44
Bastian Engel <bastian.engel00@gmail.com>
2023-08-13 03:57:39 +0200

    anti-aliasing

--- src/camera.rs
+++ src/camera.rs
@@ -8,42 +8,55 @@ pub struct Camera {
     focal_length: f64,
     vp_width: f64,
     vp_height: f64,
+    pub samples_per_pixel: u32,
     origin: Vec3,
     vp_horizontal: Vec3,
     vp_vertical: Vec3,
-    lower_left_corner: Vec3
+    pixel_delta_x: Vec3,
+    pixel_delta_y: Vec3,
+    vp_pixel00: Vec3
 }
 
 impl Camera {
-    pub fn new(height: u32, aspect_ratio: f64, focal_length: f64, viewport_height: f64, origin: Vec3) -> Camera {
+    pub fn new(height: u32, aspect_ratio: f64, focal_length: f64, vp_height: f64,
+               samples_per_pixel: u32, origin: Vec3) -> Camera {
         let width = ((height as f64) * aspect_ratio) as u32;
-        let viewport_width = viewport_height * aspect_ratio;
-        let viewport_horizontal = Vec3::RIGHT * viewport_width;
-        let viewport_vertical = -Vec3::UP * viewport_height;
-        let lower_left_corner = origin - viewport_horizontal * 0.5 - viewport_vertical * 0.5 + Vec3::FORWARD * focal_length;
+        let vp_width = vp_height * aspect_ratio;
+        let vp_horizontal = Vec3::RIGHT * vp_width;
+        let vp_vertical = -Vec3::UP * vp_height;
+        let lower_left_corner = origin - vp_horizontal * 0.5 - vp_vertical * 0.5 + Vec3::FORWARD * focal_length;
+        let pixel_delta_x = vp_horizontal * (1.0 / width as f64);
+        let pixel_delta_y = vp_vertical * (1.0 / height as f64);
+        let vp_pixel00 = lower_left_corner + (pixel_delta_x + pixel_delta_y) * 0.5;
 
         Camera {
             height,
             aspect_ratio,
             width,
             focal_length,
-            vp_width: viewport_width,
-            vp_height: viewport_height,
+            vp_width,
+            vp_height,
+            samples_per_pixel,
             origin,
-            vp_horizontal: viewport_horizontal,
-            vp_vertical: viewport_vertical,
-            lower_left_corner
+            vp_horizontal,
+            vp_vertical,
+            pixel_delta_x,
+            pixel_delta_y,
+            vp_pixel00
         }
     }
 
     pub fn generate_ray_for_pixel(&self, x: u32, y: u32) -> Ray {
-            let u = x as f64 / self.width as f64;
-            let v = y as f64 / self.height as f64;
+        let pixel_center = self.vp_pixel00 + (self.pixel_delta_x * x as f64 + self.pixel_delta_y * y as f64);
+        let sample_off_x = rand::random::<f64>() - 0.5;
+        let sample_off_y = rand::random::<f64>() - 0.5;
+        let pixel_sample = pixel_center + (self.pixel_delta_x * sample_off_x +
+                                           self.pixel_delta_y * sample_off_y);
 
-            Ray {
-                origin: self.origin,
-                direction: (self.lower_left_corner + self.vp_horizontal * u + self.vp_vertical * v).normalized()
-            }
+        Ray {
+            origin: self.origin,
+            direction: (pixel_sample - self.origin).normalized()
+        }
     }
 
     pub fn background_color(ray: &Ray) -> Vec3 {
--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,29 @@ fn main() {
     scene.geometry.push(Box::new(Sphere { origin: Vec3(0.0, 0.0, -1.0), radius: 0.5 }));
     scene.geometry.push(Box::new(Sphere { origin: Vec3(0.0, -100.5, -1.0), radius: 100.0 }));
 
-    let camera = Camera::new(1080, 16.0 / 9.0, 1.0, 2.0, Vec3::ZERO);
+    let camera = Camera::new(480, 16.0 / 9.0, 1.0, 2.0, 10, Vec3::ZERO);
 
     let mut buf: image::ImageBuffer<Rgb<u8>, _> = image::ImageBuffer::new(camera.width, camera.height);
 
     for y in 0..camera.height {
         // println!("Scanlines remaining: {}/{}", y, img_height);
         for x in 0..camera.width {
-            let ray = camera.generate_ray_for_pixel(x, y);
-            let hit = scene.cast_ray(&ray, 0.0, f64::MAX);
-            let color = match hit {
-                Some(intersect) => (intersect.normal + Vec3::UNIT) * 0.5,
-                None => Camera::background_color(&ray)
-            };
+            let mut color_sum = Vec3::ZERO;
 
-            buf.put_pixel(x, y, color.into());
+            for _ in 0..camera.samples_per_pixel {
+                let ray = camera.generate_ray_for_pixel(x, y);
+                let hit = scene.cast_ray(&ray, 0.0, f64::MAX);
+                let color = match hit {
+                    Some(intersect) => (intersect.normal + Vec3::UNIT) * 0.5,
+                    None => Camera::background_color(&ray)
+                };
+
+                color_sum += color;
+            }
+
+            color_sum *= 1.0 / camera.samples_per_pixel as f64;
+
+            buf.put_pixel(x, y, color_sum.into());
         }
     }
 
